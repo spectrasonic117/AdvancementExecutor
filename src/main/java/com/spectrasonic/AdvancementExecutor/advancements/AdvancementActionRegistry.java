9d54b23ca5711dd6f3b7d054eafa7657e1c74d6b
Spectrasonic <spectrasonic117@gmail.com>
2025-05-11 17:38:46 -0600

    ♻️ refactor advancements handling

--- src/main/java/com/spectrasonic/AdvancementExecutor/advancements/AdvancementActionRegistry.java
+++ src/main/java/com/spectrasonic/AdvancementExecutor/advancements/AdvancementActionRegistry.java
@@ -38,7 +38,62 @@ public class AdvancementActionRegistry {
                     player.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 3));
                     MessageUtils.sendMessage(player, "<green>Has recibido 3 IRON_INGOT</green>");
                 });
-
+        register("minecraft:husbandry/plant_seed",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:husbandry/fishy_business",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:husbandry/tactical_fishing",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:husbandry/breed_an_animal",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:husbandry/tame_an_animal",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:adventure/sleep_in_bed",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:adventure/ol_betsy",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:adventure/trim_with_any_armor_pattern",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:adventure/shoot_arrow",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:adventure/kill_a_mob",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:nether/obtain_blaze_rod",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:nether/find_fortress",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:nether/ride_strider",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
+        register("minecraft:nether/find_bastion",
+                player -> {
+                    MessageUtils.sendMessage(player, "<green>Logro obtenido</green>");
+                });
     }
 
     public static void register(String advancementKey, Consumer<Player> action) {
